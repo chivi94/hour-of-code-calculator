759833f11c4918b8825bc50e8ab756998c1b8e79
Ivan <ivan.gonzalez.rincon>
2018-11-24 17:57:02 +0100

    Quitados ficheros innecesarios

--- app/src/main/java/uva/inf/ivagonz/mycalculator/MainActivity.java
+++ app/src/main/java/uva/inf/ivagonz/mycalculator/MainActivity.java
@@ -1,130 +1,26 @@
 package uva.inf.ivagonz.mycalculator;
 
-import android.content.Context;
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private double sumar(double a, double b) {
-        return a + b;
-    }
-
-    private double restar(double a, double b) {
-        return a - b;
-    }
-
-    private double multiplicar(double a, double b) {
-        return a * b;
-    }
-
-    private double dividir(double a, double b) {
-        return a / b;
-    }
-
-    private double potencia(double a, double b) {
-        return Math.pow(a, b);
-    }
-
-    private void showMessage(Context context) {
-        Toast.makeText(context, "Debes introducir números", Toast.LENGTH_SHORT).show();
-    }
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //Enlace de los elementos gráficos con nuestro código
-        Button btn_sumar = (Button) findViewById(R.id.btn_sumar);
-        Button btn_restar = (Button) findViewById(R.id.btn_restar);
-        Button btn_multiplicar = (Button) findViewById(R.id.btn_multiplicar);
-        Button btn_dividir = (Button) findViewById(R.id.btn_dividir);
-
-        TextView tv_num1 = (TextView) findViewById(R.id.tv_numero1);
-        TextView tv_num2 = (TextView) findViewById(R.id.tv_numero2);
-
-
-        btn_sumar.setOnClickListener(this);
-        btn_restar.setOnClickListener(this);
-        btn_multiplicar.setOnClickListener(this);
-        btn_dividir.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        TextView tv_resultado = (TextView) findViewById(R.id.tv_resultado);
-        EditText et_num1 = (EditText) findViewById(R.id.et_numero1);
-        EditText et_num2 = (EditText) findViewById(R.id.et_numero2);
-
-        String txt_1 = et_num1.getText().toString();
-        String txt_2 = et_num2.getText().toString();
-
-        LayoutInflater li = LayoutInflater.from(MainActivity.this);
-        View ly_dialog = li.inflate(R.layout.dialog_layout, null);
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                MainActivity.this);
-
-        // set prompts.xml to alertdialog builder
-        alertDialogBuilder.setView(ly_dialog);
-        // set dialog message
-        alertDialogBuilder
-                .setCancelable(false)
-                .setPositiveButton("Ok",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                // get user input and set it to result
-                                // edit text
-                                dialog.cancel();
-                            }
-                        })
-                .setNegativeButton("Cancelar",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
-        // create alert dialog
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        final TextView tv_resul_dialog = (TextView) ly_dialog
-                .findViewById(R.id.tv_resul_dialog);
-
-
-        try {
-            switch (id) {
-                case R.id.btn_sumar:
-                    tv_resul_dialog.setText(String.valueOf(sumar(Double.parseDouble(txt_1), Double.parseDouble(txt_2))));
-                    tv_resultado.setText(String.valueOf(sumar(Double.parseDouble(txt_1), Double.parseDouble(txt_2))));
-                    break;
-                case R.id.btn_restar:
-                    tv_resul_dialog.setText(String.valueOf(restar(Double.parseDouble(txt_1), Double.parseDouble(txt_2))));
-                    tv_resultado.setText(String.valueOf(restar(Double.parseDouble(txt_1), Double.parseDouble(txt_2))));
-                    break;
-                case R.id.btn_multiplicar:
-                    tv_resul_dialog.setText(String.valueOf(multiplicar(Double.parseDouble(txt_1), Double.parseDouble(txt_2))));
-                    tv_resultado.setText(String.valueOf(multiplicar(Double.parseDouble(txt_1), Double.parseDouble(txt_2))));
-                    break;
-                case R.id.btn_dividir:
-                    tv_resul_dialog.setText(String.valueOf(dividir(Double.parseDouble(txt_1), Double.parseDouble(txt_2))));
-                    tv_resultado.setText(String.valueOf(dividir(Double.parseDouble(txt_1), Double.parseDouble(txt_2))));
-                    break;
-            }
-            // show it
-            alertDialog.show();
-        } catch (NumberFormatException e) {
-            showMessage(MainActivity.this);
+        if (savedInstanceState == null) {
+            // Crear un fragment
+            CalculatorFragment fragment = new CalculatorFragment();
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, fragment,
+                            fragment.getClass().getSimpleName())
+                    .commit();
         }
     }
+
 }
